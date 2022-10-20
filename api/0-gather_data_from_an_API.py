08ec665789fca68f91b04ff7c6b74de7ad327509
Vanessa Peggy Otieno <61325877+vassa33@users.noreply.github.com>
2022-10-20 23:25:19 +0300

    Gather data from API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -22,6 +22,7 @@ if __name__ == '__main__':
                                  (obj["completed"] is True), todos_info))
     number_of_done_tasks = len(task_completed)
     total_number_of_tasks = len(todos_info)
+    
 
     print("Employee {} is done with tasks({}/{}):".
           format(employee_name, number_of_done_tasks, total_number_of_tasks))
